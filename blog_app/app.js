02fb2c2d2977eb959f25573e25b0f9f41c411c2f
DiptoChakrabarty <diptochuck123@gmail.com>
2019-12-28 11:15:51 +0530

    initialised blogs

--- blog_app/app.js
+++ blog_app/app.js
@@ -21,8 +21,34 @@ var blogschema = new mongoose.Schema({
 
 var blog = mongoose.model("blog",blogschema);
 
-// setup REST routes
+blog.create({
+    title: 'sam', 
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHys3zUHtG3crOhn9e93gdB1g1K6fS-628jLZ9os6btto4gfRZ&s",
+    body: "Awesome Stealth Game"
+}, function(err,blog){
+    if(err){
+        console.log(err);
+    } else {
+        console.log("New blog added");
+        console.log(blog);
+    }
+})*
 
+// setup REST routes
+app.get("/",function(req,res){
+    res.redirect("/blogs");
+});
+
+app.get("/blogs",function(req,res){
+    blog.find({},function(err,blogs){
+        if(err){
+            console.log("Error");
+        } else {
+            res.render("index" , {blogs : blogs})
+        }
+    });
+   
+});
 
 
 app.listen(3000,function(){
