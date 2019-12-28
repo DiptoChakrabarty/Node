3c096d5f686c9b12e4a0067888db5d727c276cc8
DiptoChakrabarty <diptochuck123@gmail.com>
2019-12-28 09:54:54 +0530

    db cleared

--- yelpcamp/y1/app.js
+++ yelpcamp/y1/app.js
@@ -12,14 +12,16 @@ app.set("view engine","ejs");
 // Set Schema
 var gameschema = new mongoose.Schema({
     name: String,
-    url: String
+    url: String,
+    description: String
 });
 
 var games = mongoose.model("games",gameschema);
 
 games.create({
     name: 'sam', 
-    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHys3zUHtG3crOhn9e93gdB1g1K6fS-628jLZ9os6btto4gfRZ&s"
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHys3zUHtG3crOhn9e93gdB1g1K6fS-628jLZ9os6btto4gfRZ&s",
+    description: "Awesome Stealth Game"
 }, function(err,game){
     if(err){
         console.log(err);
@@ -56,14 +58,26 @@ app.post("/game",function(req,res){
     var name = req.body.name;
     var url = req.body.url;
     var newgame= {name: name , url: url};
-    games.push(newgame);
-    res.redirect("/game")
+    //games.push(newgame);
+    // save to db
+    games.create(newgame,function(err,newimg){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/game");
+        }
+    });
+    
 });
 
 app.get("/new",function(req,res){
           
     res.render("form")
 });
+
+app.get("/game/:id",function(req,res){
+    res.get("New id page");
+})
 
 
 app.listen(3000,function(){
