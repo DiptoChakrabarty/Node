6f984f6bce64711c4283e76bf829583cf54b9eac
DiptoChakrabarty <diptochuck123@gmail.com>
2020-02-19 23:48:52 +0530

    func def

--- noteapp/notes.js
+++ noteapp/notes.js
@@ -1,9 +1,27 @@
+const fs =  require("fs")
+
 const getnotes = function(){
     return 'Your notes ...'
 }
 
 
 const addnotes = function(title,body){
+    const notes = loadNotes()
+
+}
+
+
+const loadNotes = function () {
+
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+    const dataJson = dataBuffer.toString()
+    return JSON.parse(dataJson)
+
+    }catch(e){
+        return []
+    }
+    
 
 }
 
